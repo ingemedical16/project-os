c3b163eb0bf0901c1fbbd9d6a8df1db0d1831e8f
Mohammed El-IDRISSI <ingmedical16@gmail.com>
2026-06-06 08:01:30 +0000

    refactor: organize ui components by category

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -28,3 +28,33 @@ export * from './navigation/Tabs';
 export * from './display/Card';
 export * from './display/Avatar';
 export * from './display/Divider';
+// Forms
+export * from './forms/Button';
+export * from './forms/Input';
+export * from './forms/Textarea';
+export * from './forms/Select';
+export * from './forms/Checkbox';
+export * from './forms/Radio';
+export * from './forms/RadioGroup';
+export * from './forms/Switch';
+export * from './forms/Form';
+
+// Feedback
+export * from './feedback/Alert';
+export * from './feedback/Badge';
+export * from './feedback/Spinner';
+export * from './feedback/Toast';
+
+// Overlay
+export * from './overlay/Modal';
+export * from './overlay/Drawer';
+export * from './overlay/Tooltip';
+
+// Navigation
+export * from './navigation/Accordion';
+export * from './navigation/Tabs';
+
+// Display
+export * from './display/Card';
+export * from './display/Avatar';
+export * from './display/Divider';
